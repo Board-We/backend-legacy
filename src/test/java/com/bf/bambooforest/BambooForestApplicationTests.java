2baf55be2dc8d5f941b63c9b536491e7ad71ac77
jjungyeun <wjddusdl11@ajou.ac.kr>
2022-09-15 19:32:37 +0900

    [feat] 프로젝트 생성

---
+++ src/test/java/com/bf/bambooforest/BambooForestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bf.bambooforest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BambooForestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
