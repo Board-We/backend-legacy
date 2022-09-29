3d24f3c487ddbfc6e58079752a876509c2364dff
jcdad3000 <31876648+jcdad3000@users.noreply.github.com>
2022-09-29 20:23:18 +0900

    [feat] PK에 GenerateValue 설정 추가

--- src/main/java/com/bf/bambooforest/entity/User.java
+++ src/main/java/com/bf/bambooforest/entity/User.java
@@ -4,12 +4,13 @@ import com.sun.istack.NotNull;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class User {
 
-    @Id
+    @Id @GeneratedValue
     @Column(name="USER_ID")
     private Long id;
 
