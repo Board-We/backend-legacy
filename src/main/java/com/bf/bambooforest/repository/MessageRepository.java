049b6cdf23dbc8f5f5afd805ee43d66aff77fa8b
soupbab <2012103483@khu.ac.kr>
2022-10-06 19:47:27 +0900

    [feat] MessageService의 getMessageDto 구현
    - db에서 데이터를 읽어올 수 있도록 서비스 로직 구현

--- src/main/java/com/bf/bambooforest/repository/MessageRepository.java
+++ src/main/java/com/bf/bambooforest/repository/MessageRepository.java
@@ -6,4 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, Integer> {
+
+    List<Message> findAllByUserId(Long userId);
+
 }
--- src/main/java/com/bf/bambooforest/service/MessageService.java
+++ src/main/java/com/bf/bambooforest/service/MessageService.java
@@ -1,22 +1,42 @@
 package com.bf.bambooforest.service;
 
 import com.bf.bambooforest.dto.GetMessagesResponseDto;
+import com.bf.bambooforest.entity.Message;
+import com.bf.bambooforest.entity.User;
+import com.bf.bambooforest.repository.MessageRepository;
+import com.bf.bambooforest.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class MessageService {
 
+    private final UserRepository userRepository;
 
-    public GetMessagesResponseDto getMessageDto(String phoneNumber) {
-                return GetMessagesResponseDto.builder()
-                .messages(List.of("hello", "world"))
-                .build();
-    }
+    private final MessageRepository messageRepository;
 
     public void sendMessage(String phoneNumber, String message) {
         System.out.println("phoneNumber = " + phoneNumber);
         System.out.println("message = " + message);
+    }
+
+    public GetMessagesResponseDto getMessageDto(String phoneNumber) {
+        List<String> messages = new ArrayList<>();
+        Optional<User> userOptional = userRepository.findByPhoneNumber(phoneNumber);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            List<Message> foundMessages = messageRepository.findAllByUserId(user.getId());
+            for (Message message : foundMessages) {
+                messages.add(message.getContent());
+            }
+        }
+        return GetMessagesResponseDto.builder()
+                .messages(messages)
+                .build();
     }
 }
