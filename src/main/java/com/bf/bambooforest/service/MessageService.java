0ce825d6c6c4452844a8245cdb4e3937ed55a123
jcdad3000 <31876648+jcdad3000@users.noreply.github.com>
2022-10-13 19:30:48 +0900

    [feat] 메시지 read시 STATUS=UNREAD 를 가져옴
    - MessageRepository 에 UserId와 Status를 이용하여 메시지를 가져오는 JPA 메소드 쿼리로 변경
    - getMessageDto 메소드 @Transactional readonly = False로 변경 , DB write logic이 추가되었기 때문에 변경함

--- src/main/java/com/bf/bambooforest/service/MessageService.java
+++ src/main/java/com/bf/bambooforest/service/MessageService.java
@@ -2,6 +2,7 @@ package com.bf.bambooforest.service;
 
 import com.bf.bambooforest.dto.GetMessagesResponseDto;
 import com.bf.bambooforest.entity.Message;
+import com.bf.bambooforest.entity.MessageStatus;
 import com.bf.bambooforest.entity.User;
 import com.bf.bambooforest.repository.MessageRepository;
 import com.bf.bambooforest.repository.UserRepository;
@@ -42,12 +43,13 @@ public class MessageService {
 
     }
 
+    @Transactional
     public GetMessagesResponseDto getMessageDto(String phoneNumber) {
         List<String> messages = new ArrayList<>();
         Optional<User> userOptional = userRepository.findByPhoneNumber(phoneNumber);
         if (userOptional.isPresent()) {
             User user = userOptional.get();
-            List<Message> foundMessages = messageRepository.findAllByUserId(user.getId());
+            List<Message> foundMessages = messageRepository.findByUserIdAndStatus(user.getId(), MessageStatus.UNREAD);
             for (Message message : foundMessages) {
                 message.read();
                 messages.add(message.getContent());
